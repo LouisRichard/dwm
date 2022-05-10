9f13f09ba85a2a4223a8072aa3455e8e2f97129d
LouisRichard <louis.richard@tutanota.com>
2022-05-10 23:44:51 +0200

    modified fonts and controls

--- config.h
+++ config.h
@@ -7,7 +7,7 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=12", "fontawesome:size=12" };
+static const char *fonts[]          = { "InconsolataGo Nerd Font:style=regular:size=12" };
 static const char dmenufont[]       = "monospace:size=11";
 //background color
 static const char col_gray1[]       = "#222222";
